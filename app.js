ab3a5329e82da0e702882ce8c9abee5318911ba9
Himanshu Maan <maanhimanshu1000@gmail.com>
2024-08-12 13:56:07 +0530

    second commit

--- app.js
+++ app.js
@@ -8,16 +8,22 @@ const cors = require("cors");
 
 require("dotenv").config();
 require("./conn/conn");
+
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
-    credentials: true,
+    origin: [
+      "http://localhost:5173",          // Development origin
+      "https://flourishing-sopapillas-1277e0.netlify.app" // Production origin
+    ],
+    credentials: true, // Allows cookies to be sent with requests
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
-//all routes
+
+// All routes
 app.use("/api/v1", userApi);
 app.use("/api/v1", CatApi);
 app.use("/api/v1", PodcastApi);
